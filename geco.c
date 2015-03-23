4daa2a4a17a5d7428c8165ca2db133bd7449bebd
pratas <pratas@ua.pt>
2015-03-23 15:59:32 +0000

    F354

--- geco.c
+++ geco.c
@@ -69,12 +69,7 @@ refNModels, INF *I){
   totModels = P->nModels;
   for(n = 0 ; n < P->nModels ; ++n) 
     if(P->model[n].edits != 0)
-//#define HIGHX
-#ifdef HIGHX
-      totModels += 2; // SUBS, ADDS
-#else
       totModels += 1;
-#endif
 
   pModel        = (PModel  **) Calloc(totModels, sizeof(PModel *));
   for(n = 0 ; n < totModels ; ++n)
@@ -160,35 +155,7 @@ refNModels, INF *I){
         cModels[cModel]->alphaDen);
         ComputeWeightedFreqs(cModelWeight[n], pModel[n], PT);
 
-#ifdef HIGHX
-        ++n;
-        ComputePModel(cModels[cModel], pModel[n], cModels[cModel]->pModelIdx,
-        1);
-
-        uint8_t control = 0, maxi = 0, maxp = pModel[n]->freqs[0];
-        for(x = 1 ; x < 4 ; ++x)
-          if(pModel[n]->freqs[x] > maxp){
-            maxp = pModel[n]->freqs[x];
-            maxi = x;
-            }
-
-        for(x = 0 ; x < 4 ; ++x) 
-          if(maxi != x && maxp == pModel[n]->freqs[x]) 
-            control = 101;
-
-        if(control != 101){
-          for(x = 0 ; x < 4 ; ++x)
-            if(x == maxi)
-              pModel[n]->freqs[x] = 2000;
-            else
-              pModel[n]->freqs[x] = 1;        
-          }
-
-        ComputeWeightedFreqs(cModelWeight[n], pModel[n], PT);
-#endif
-
         if(cModels[cModel]->edits != 0){
-          // SUBSTITUTIONS HANDLING: - - - - - - - - - - - - - - - - - - - - - -
           ++n;
           cModels[cModel]->SUBS.seq->buf[cModels[cModel]->SUBS.seq->idx] = sym;
           cModels[cModel]->SUBS.idx = GetPModelIdxCorr(cModels[cModel]->SUBS.
@@ -197,29 +164,6 @@ refNModels, INF *I){
           ComputePModel(cModels[cModel], pModel[n], cModels[cModel]->SUBS.idx, 
           cModels[cModel]->SUBS.eDen);
           ComputeWeightedFreqs(cModelWeight[n], pModel[n], PT);
-/*
-          // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-          // TODO: ADDITIONS
-          ++n;
-          cModels[cModel]->ADDS.idx2 = cModels[cModel]->ADDS.idx;
-          cModels[cModel]->ADDS.seq->buf[cModels[cModel]->ADDS.seq->idx] = sym;
-          cModels[cModel]->ADDS.idx = GetPModelIdxCorr(cModels[cModel]->ADDS.
-          seq->buf+cModels[cModel]->ADDS.seq->idx-1, cModels[cModel], cModels
-          [cModel]->ADDS.idx);
-          ComputePModel(cModels[cModel], pModel[n], cModels[cModel]->ADDS.idx, 100);
-          ComputeWeightedFreqs(cModelWeight[n], pModel[n], PT);
-          // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-          // TODO: DELETIONS
-          ++n;
-          cModels[cModel]->DELS.idx2 = cModels[cModel]->DELS.idx;
-          cModels[cModel]->DELS.seq->buf[cModels[cModel]->DELS.seq->idx] = sym;
-          cModels[cModel]->DELS.idx = GetPModelIdxCorr(cModels[cModel]->DELS.
-          seq->buf+cModels[cModel]->DELS.seq->idx-1, cModels[cModel], cModels
-          [cModel]->DELS.idx);
-          ComputePModel(cModels[cModel], pModel[n], cModels[cModel]->DELS.idx, 100);
-          ComputeWeightedFreqs(cModelWeight[n], pModel[n], PT);
-          // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-*/
           }
 
         ++n;
@@ -260,8 +204,6 @@ refNModels, INF *I){
       for(cModel = 0 ; cModel < P->nModels ; ++cModel){
         if(cModels[cModel]->edits != 0){
           CorrectCModelSUBS(cModels[cModel], pModel[++n], sym);
-          //CorrectCModelADDS(cModels[cModel], pModel[++n], sym);
-          //CorrectCModelDELS(cModels[cModel], pModel[++n], sym);
           }
         ++n;
         }
